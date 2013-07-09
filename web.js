c86e1ddf51419c2cf279d45307864e349234f796
Rajesh Radhakrishnan <enthoughts@gmail.com>
2013-07-09 01:08:04 +0000

    added read from index.html

--- web.js
+++ web.js
@@ -3,19 +3,15 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var fileContents = new Buffer(100);
     var fs = require('fs');
-    fs.readFileSync("./index.html","ascii", function read(err,data) {
+    var filename = fs.readFileSync("./index.html","ascii", function read(err,data) {
 	if (err) {
 	    return console.log(err);
 	}
-
-	fileContents.write(data,"ascii");
-	console.log(fileContents.toString("ascii"));
-    });
-		    
-    console.log(fileContents.toString("ascii"));
-    response.send(fileContents.toString("ascii"));
+    })
+    var buf = new Buffer(filename);
+    console.log(buf.toString("ascii"));
+    response.send(buf.toString("ascii"));
 });
 
 var port = process.env.PORT || 5000;
