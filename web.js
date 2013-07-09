fa6a8df8271e71af238ea464193aa44cb0c052f5
Rajesh Radhakrishnan <enthoughts@gmail.com>
2013-07-09 00:40:05 +0000

    added read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,16 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var fileContents;
+    var fs = require('fs');
+    fs.readFileSync('./index.html',function read(err,data) {
+	if (err) {
+	    return console.log(err);
+	}
+	fileContents =  new Buffer(data);
+    });
+		    
+    response.send(fileContents.toString());
 });
 
 var port = process.env.PORT || 5000;
